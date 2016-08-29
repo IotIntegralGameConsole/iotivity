c3ece2aff86d8e68edfa326caddf692e8d496ec8
Philippe Coval <philippe.coval@osg.samsung.com>
2016-08-29 16:59:34 +0200

    build: Prepare building by installing dependencies libs
    It is strongly recommended to rely on scons instead,
     if not possible, this can be done differently using this script.
    
    For more details about building IoTivity please check:
    
    https://wiki.iotivity.org/build
    
    Bug: https://jira.iotivity.org/browse/IOT-820
    Change-Id: I63e713c8531f0eea698277993cf21c3d55c80270
    Origin: https://gerrit.iotivity.org/gerrit/#/c/11699/
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

---
+++ prep.sh
@@ -0,0 +1,77 @@
+#!/bin/sh
+
+set -f
+
+[ ! -z ${EXEC_MODE} ] || EXEC_MODE=false
+
+
+do_()
+{
+    set +f
+    if $EXEC_MODE; then
+        echo "warning: fetching online sources may not be reproductible"
+        printf "%s\n" "trying: \"$@\""
+        eval "$@"
+    else
+        echo "# Please manually execute from shell:"
+        printf "%s\n" "$@"
+    fi
+    set -f
+}
+
+
+main_()
+{
+    cat<<EOF
+#
+# WARNING: This helper script is DISCOURAGED to be used
+#
+#  Instead please use the supported build tool "scons"
+#
+#  However in some specific cases it's not possible
+#  so this script is providing a fallback option.
+#
+#  Reminder: it's not safe to access online resources at build time,
+#  so by default the script will tell what to do (like scons),
+#  to enable tasks executions it should be run with EXEC_MODE enabled:
+#
+#  EXEC_MODE=true ./prep.sh
+#
+#  More at:
+#  https://jira.iotivity.org/browse/IOT-820
+#
+EOF
+
+    which git > /dev/null
+    which unzip > /dev/null
+    which wget > /dev/null
+
+    echo "# Checking for tinycbor presence:"
+    tinycbor_url='https://github.com/01org/tinycbor.git'
+    tinycbor_rev='v0.2.1'
+    if [ ! -e 'extlibs/tinycbor/tinycbor' ] ; then
+        do_ "git clone -b "${tinycbor_rev}" "$tinycbor_url" extlibs/tinycbor/tinycbor"
+    fi
+
+    echo "# Checking for gtest presence:"
+    gtest_url='http://pkgs.fedoraproject.org/repo/pkgs/gtest/gtest-1.7.0.zip/2d6ec8ccdf5c46b05ba54a9fd1d130d7/gtest-1.7.0.zip'
+    gtest_dir="extlibs/gtest/"
+    gtest_archive=$(basename -- "${gtest_url}")
+    gtest_file="${gtest_dir}/gtest-1.7.0/CMakeLists.txt"
+    if [ ! -e "${gtest_file}" ] ; then
+        do_ "cd ${gtest_dir} && wget -nc -O ${gtest_url} && unzip ${gtest_archive}"
+    fi
+
+    echo "# Checking for sqlite presence:"
+    sqlite_url='http://www.sqlite.org/2015/sqlite-amalgamation-3081101.zip'
+    sqlite_dir="extlibs/sqlite3"
+    sqlite_file="${sqlite_dir}/sqlite3.c"
+    sqlite_archive=$(basename -- "${sqlite_url}")
+    if [ ! -e 'extlibs/sqlite3/$sqlite_file' ] ; then
+        do_ "cd ${sqlite_dir} && wget -nc $sqlite_url && unzip ${sqlite_archive} && mv */* ."
+    fi
+}
+
+
+main_
+
