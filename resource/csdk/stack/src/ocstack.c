afc6f86f6097c4a195c9f21383c042e3ef9a2673
Abhishek Sharma <ce.abhishek@samsung.com>
2016-09-19 17:16:34 +0530

    Remove WITH_CHPROXY flag for build cleanup
    To minimize feature specific build flags,
    removed proxy flag from base layer.
    
    Change-Id: Ic3498e181209b31aa7d531f1eb6f86a2fc92ba54
    Signed-off-by: Abhishek Sharma <ce.abhishek@samsung.com>
    Reviewed-on: https://gerrit.iotivity.org/gerrit/11937
    Tested-by: jenkins-iotivity <jenkins-iotivity@opendaylight.org>
    Reviewed-by: Uze Choi <uzchoi@samsung.com>
    Reviewed-by: Ziran Sun <ziran.sun@samsung.com>

--- resource/csdk/stack/src/ocstack.c
+++ resource/csdk/stack/src/ocstack.c
@@ -4781,12 +4781,10 @@ bool OCResultToSuccess(OCStackResult ocResult)
     }
 }
 
-#ifdef WITH_CHPROXY
 OCStackResult OCSetProxyURI(const char *uri)
 {
     return CAResultToOCResult(CASetProxyUri(uri));
 }
-#endif
 
 #if defined(RD_CLIENT) || defined(RD_SERVER)
 OCStackResult OCBindResourceInsToResource(OCResourceHandle handle, uint8_t ins)
