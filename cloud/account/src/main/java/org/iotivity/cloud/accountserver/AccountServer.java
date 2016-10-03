356fc353815137ee2eec28e3ce27e84a7f6311d2
Sunil Kumar K R <sunil.k14@samsung.com>
2016-10-03 12:32:40 +0530

    Enable AclVerifyResource line added
    Change-Id: Idbc16ae7893ce57eb4f5b0e7473693dab2a05288
    Signed-off-by: Sunil Kumar K R <sunil.k14@samsung.com>
    Reviewed-on: https://gerrit.iotivity.org/gerrit/12717
    Reviewed-by: Ashwini  Kumar <k.ashwini@samsung.com>
    Tested-by: jenkins-iotivity <jenkins-iotivity@opendaylight.org>
    Reviewed-by: dongik Lee <dongik.lee@samsung.com>
    Reviewed-by: Randeep Singh <randeep.s@samsung.com>
    (cherry picked from commit 76286f9dae19891813b0bba9af33b646415ec86b)
    Reviewed-on: https://gerrit.iotivity.org/gerrit/12801

--- cloud/account/src/main/java/org/iotivity/cloud/accountserver/AccountServer.java
+++ cloud/account/src/main/java/org/iotivity/cloud/accountserver/AccountServer.java
@@ -29,6 +29,7 @@ import org.iotivity.cloud.accountserver.resources.account.session.SessionResourc
 import org.iotivity.cloud.accountserver.resources.account.tokenrefresh.TokenRefreshResource;
 import org.iotivity.cloud.accountserver.resources.acl.group.GroupResource;
 import org.iotivity.cloud.accountserver.resources.acl.id.AclResource;
+import org.iotivity.cloud.accountserver.resources.acl.verify.AclVerifyResource;
 import org.iotivity.cloud.accountserver.resources.acl.invite.InviteResource;
 import org.iotivity.cloud.accountserver.resources.certificate.CertificateResource;
 import org.iotivity.cloud.base.ServerSystem;
@@ -63,6 +64,8 @@ public class AccountServer {
         serverSystem.addResource(new GroupResource());
 
         serverSystem.addResource(new AclResource());
+
+        serverSystem.addResource(new AclVerifyResource());
 
         serverSystem.addResource(new CertificateResource());
 
