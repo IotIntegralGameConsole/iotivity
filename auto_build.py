93524b2e0cf69b2b38cd696e9e2e931ede8b483a
Philippe Coval <philippe.coval@osg.samsung.com>
2016-11-30 23:14:03 +0100

    build: Extend unit_tests to all (including service)
    Bug: https://jira.iotivity.org/browse/IOT-1486
    Change-Id: I89426d04e0b05c49470c059ffe60cf90fcb95582
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>
    Reviewed-on: https://gerrit.iotivity.org/gerrit/14971
    Tested-by: jenkins-iotivity <jenkins-iotivity@opendaylight.org>
    Reviewed-by: Ziran Sun <ziran.sun@samsung.com>
    Reviewed-by: Mats Wichmann <mats@osg.samsung.com>
    Reviewed-by: George Nash <george.nash@intel.com>

--- auto_build.py
+++ auto_build.py
@@ -469,14 +469,7 @@ def unit_tests():
     build_options = {
                         'RELEASE':'false',
                     }
-    extra_option_str = "resource -c"
-    call_scons(build_options, extra_option_str)
-
-    build_options = {
-                        'LOGGING':'false',
-                        'RELEASE':'false',
-                    }
-    extra_option_str = "resource"
+    extra_option_str = "-c ."
     call_scons(build_options, extra_option_str)
 
     build_options = {
@@ -484,6 +477,7 @@ def unit_tests():
                         'RELEASE':'false',
                     }
     call_scons(build_options, extra_option_str)
+
     build_options = {
                         'TEST':1,
                         'SECURED':1,
