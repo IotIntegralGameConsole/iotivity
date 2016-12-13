330cadcfa03af39ac978cfb9a1d5e28fe954fc38
jihwan.seo <jihwan.seo@samsung.com>
2016-12-13 13:49:57 +0900

    removed unreachable code in android JNI
    Change-Id: Ie193980b00bbab996357c9011d15e9f612fd32b0
    Signed-off-by: jihwan.seo <jihwan.seo@samsung.com>
    Reviewed-on: https://gerrit.iotivity.org/gerrit/15543
    Tested-by: jenkins-iotivity <jenkins-iotivity@opendaylight.org>
    Reviewed-by: Dan Mihai <Daniel.Mihai@microsoft.com>
    Reviewed-by: Larry Sachs <larry.j.sachs@intel.com>
    Reviewed-by: Ashok Babu Channa <ashok.channa@samsung.com>

--- android/android_api/base/jni/JniUtils.h
+++ android/android_api/base/jni/JniUtils.h
@@ -56,7 +56,7 @@ public:
             default:
                 ThrowOcException(OC_STACK_INVALID_PARAM, "Unexpected service type");
                 return OC::ServiceType::OutOfProc;
-        };
+        }
     }
 
     static OC::ModeType getModeType(JNIEnv *env, int type)
@@ -74,7 +74,7 @@ public:
             default:
                 ThrowOcException(OC_STACK_INVALID_PARAM, "Unexpected mode type");
                 return OC::ModeType::Both;
-        };
+        }
     }
 
     static OC::QualityOfService getQOS(JNIEnv *env, int type)
@@ -92,7 +92,7 @@ public:
             default:
                 ThrowOcException(OC_STACK_INVALID_PARAM, "Unexpected quality of service");
                 return OC::QualityOfService::NaQos;
-        };
+        }
     }
 
     static OC::ObserveType getObserveType(JNIEnv *env, int type)
@@ -106,7 +106,7 @@ public:
             default:
                 ThrowOcException(OC_STACK_INVALID_PARAM, "Unexpected observe type");
                 return OC::ObserveType::ObserveAll;
-        };
+        }
     }
 #ifdef WITH_CLOUD
     static OC::AclGroupType getAclGroupType(JNIEnv *env, int type)
@@ -144,7 +144,7 @@ public:
             default:
                 ThrowOcException(OC_STACK_INVALID_PARAM, "Unexpected OCEntityHandlerResult");
                 return OCEntityHandlerResult::OC_EH_ERROR;
-        };
+        }
     }
 
     static std::string stackResultToStr(const int result)
