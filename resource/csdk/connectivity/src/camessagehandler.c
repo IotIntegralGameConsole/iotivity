27d42c901f45da6cf922564fa8c672d87deb09ab
Philippe Coval <philippe.coval@osg.samsung.com>
2017-02-22 23:51:57 +0100

    WIP: mod: resource/csdk/connectivity/src/camessagehandler.c (master)
    Change-Id: Ibe1535263d1b48468314b039dde78cdf77a47bb0
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

--- resource/csdk/connectivity/src/camessagehandler.c
+++ resource/csdk/connectivity/src/camessagehandler.c
@@ -66,7 +66,8 @@ static CAQueueingThread_t g_receiveThread;
 #endif  // SINGLE_THREAD
 
 #define TAG "OIC_CA_MSG_HANDLE"
-
+#undef OIC_LOG_V
+#define OIC_LOG_V OIC_LOG_V_
 static CARetransmission_t g_retransmissionContext;
 
 // handler field
