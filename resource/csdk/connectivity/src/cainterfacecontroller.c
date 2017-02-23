6310ebc2e8d6a8d314fa71c61c32105525dc7a6e
Philippe Coval <philippe.coval@osg.samsung.com>
2017-02-23 13:15:09 +0100

    WIP: mod: resource/csdk/connectivity/src/cainterfacecontroller.c (master)
    Change-Id: Ia107a67a76c131c45c1647702409e31c3a45f1aa
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

--- resource/csdk/connectivity/src/cainterfacecontroller.c
+++ resource/csdk/connectivity/src/cainterfacecontroller.c
@@ -53,7 +53,7 @@
 
 static CAConnectivityHandler_t *g_adapterHandler = NULL;
 
-static uint32_t g_numberOfAdapters = 0;
+static size_t g_numberOfAdapters = 0;
 
 static CANetworkPacketReceivedCallback g_networkPacketReceivedCallback = NULL;
 
