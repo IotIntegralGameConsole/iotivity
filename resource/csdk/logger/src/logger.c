8842d2136804c71952a40bf46b37e19d24006117
Philippe Coval <philippe.coval@osg.samsung.com>
2017-02-27 20:27:57 +0100

    WIP: mod: resource/csdk/logger/src/logger.c (sandbox/pcoval/on/master/arduino)
    Change-Id: I166ad2966a1232abe19840362842f53943ed7ec7
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

--- resource/csdk/logger/src/logger.c
+++ resource/csdk/logger/src/logger.c
@@ -556,4 +556,30 @@ void OCLogv(LogLevel level, const char *tag, const __FlashStringHelper *format, 
     va_end(ap);
 }
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+void OICLogf(const char *format, ...)
+{
+    static char line[80];
+    va_list args;
+    va_start(args, format);
+    int len = vsnprintf(line, sizeof(line), format, args);
+    va_end(args);
+    for (char *p = &line[0]; *p; p++) {
+        if (*p == '\n') {
+            Serial.write('\r');
+        }
+        Serial.write(*p);
+    }
+    if ((size_t) len >= sizeof(line))
+        Serial.write('$');
+}
+
+#ifdef __cplusplus
+}
+#endif
 #endif //ARDUINO
+
